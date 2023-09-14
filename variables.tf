d322233f95efa704d58e9b815f7fce923818840c
JD Garrison <69910463+jdgarrison1898@users.noreply.github.com>
2023-09-14 07:24:59 -0500

    add tfvars

--- variables.tf
+++ variables.tf
@@ -5,12 +5,11 @@
 
 variable "region" {
   description = "AWS region for all resources."
-
   type    = string
   default = "us-east-1"
 }
-#variable "account_id"{
-#  type        = string
-#  description = "The account ID in which to create/manage resources"
-#  default     = "AWS_ACCOUNT_TO_DEPLOY_TO" //target account for deployment if running this terraform locally
-#}
+variable "account_id"{
+  type        = string
+  description = "The account ID in which to create/manage resources"
+  sensitive   = true
+}
