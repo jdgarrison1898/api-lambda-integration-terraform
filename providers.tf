917e9ffd6fd669302a7450f2577f3d8353177443
jdgarrison1898 <jd.garrison@icloud.com>
2024-05-21 06:19:59 -0500

    updates

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0.0"
+      version = ">= 5.37.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.0.0"
+      version = ">= 2.0.0"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -6,7 +6,6 @@
 variable "region" {
   description = "AWS region for all resources."
   type    = string
-  default = "us-east-1"
 }
 variable "account_id"{
   type        = string
